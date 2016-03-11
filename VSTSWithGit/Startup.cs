2c7f14f073c00f99b15ac700bebe934f4f144368
Edwin Gomes <egomes3@csc.com>
2016-03-11 23:14:28 +0000

    Initial Commit

---
+++ VSTSWithGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VSTSWithGit.Startup))]
+namespace VSTSWithGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
